64b2683c83974d9bea7c28104c2a4c02665c7560
Anthony <anthony@MacBook-Pro-3.local>
2026-05-06 14:30:16 +0300

    Remove type definitions from lib

--- lisp/const.lisp
+++ lisp/const.lisp
@@ -265,14 +265,3 @@
           (alter! i (+ i 6))))
 
         ok))))
-
-(letype String [Char])
-(letype Vector (vector))
-(letype Integer Int)
-(letype Boolean Bool)
-(letype Decimal Dec)
-
-(letype Vector/Integer (vector Int))
-(letype Vector/String (vector (String)))
-(letype Vector/Boolean (vector (Boolean)))
-(letype Vector/Decimal (vector (Decimal)))
